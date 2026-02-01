b6b5e771a9daf5967d264a961ac8cb32f220be18
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-02 01:40:00 +0530

    add student register DTo

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace backend.Models
+{
+    public class Student
+    {
+        public int st_id { get; set; }
+        public int st_user_id {  get; set; }
+        public string st_registation_no { get; set;}=string.Empty;
+        public string st_fullname { get; set; } = string.Empty;
+        public string faculty { get; set; } = string.Empty;
+        public string department { get; set; } = string.Empty;
+
+        public string academic_year { get; set; } = string.Empty;
+        public string st_email { get; set; } = string.Empty;
+
+
+    }
+}
+ 
