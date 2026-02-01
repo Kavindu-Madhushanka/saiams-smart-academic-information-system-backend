8ad8c575ee706aced19cf32833614040f2509159
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-02 01:40:28 +0530

    add

---
+++ DTOs/StudentRegister.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.DTOs
+{
+    public class StudentRegister
+    {
+        public string studentName {  get; set; }=string.Empty;
+        public string registationNumber { get; set; } = string.Empty;
+        public string faculty { get; set; } = string.Empty;
+        public string department {  get; set; } = string.Empty;
+        public string academicYear {  get; set; } = string.Empty;
+        public string email {  get; set; } = string.Empty;
+        public string password {  get; set; } = string.Empty;
+    }
+}
