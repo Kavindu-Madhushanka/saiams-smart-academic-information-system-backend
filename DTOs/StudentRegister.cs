56830e5e333dabf7f70c53c2b7660373c6f4e70d
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-03 01:00:32 +0530

    modify

--- DTOs/StudentRegister.cs
+++ DTOs/StudentRegister.cs
@@ -9,5 +9,6 @@
         public string academicYear {  get; set; } = string.Empty;
         public string email {  get; set; } = string.Empty;
         public string password {  get; set; } = string.Empty;
+        public string role {  get; set; } = string.Empty;
     }
 }
