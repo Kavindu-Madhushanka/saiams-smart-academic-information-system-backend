1057d8b8b500438aed8adcf4d188109b2b878c5f
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-03 00:59:51 +0530

    modify

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,10 @@
-﻿using backend.DTOs;
+﻿using backend.Data;
+using backend.DTOs;
+using backend.Models;
 using backend.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace backend.Controllers
 {
@@ -9,10 +13,12 @@ namespace backend.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly AppDbContext _context;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService,AppDbContext context)
         {
             _authService = authService;
+            _context = context;
         }
 
         [HttpPost("login")]
@@ -31,6 +37,51 @@ namespace backend.Controllers
                 username = user.Username,
                 role = user.Role
             });
+        }
+
+        [HttpPost("registerStudent")]
+        public async Task<IActionResult> RegisterStudent([FromBody] StudentRegister register)
+        {
+            if (register == null) return BadRequest("data is null");
+
+            var logindata = new User()
+            {
+                Username = register.registationNumber,
+                PasswordHash = register.password,
+                Role = register.role,
+
+            };
+
+            _context.Users.Add(logindata);
+            await _context.SaveChangesAsync();
+
+            var newStudent = new Student()
+            {
+                st_fullname = register.studentName,
+                st_email=register.email,
+                st_registation_no=register.registationNumber,
+                department=register.department,
+                faculty=register.faculty,
+                academic_year=register.academicYear,
+                st_user_id = logindata.Id
+            };
+
+            _context.student.Add(newStudent);
+           
+
+
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Registration successful!" });
+
+
+        }
+
+        [HttpGet("getStudent")]
+        public async Task<IActionResult> GetStudent() {
+            var students = await _context.student.ToListAsync();
+            return Ok(students);
         }
     }
 }
