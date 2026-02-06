c00ac7b58ff29ed6febc24f0b0bd680591b1661e
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-06 11:59:18 +0530

    add new DTO

---
+++ DTOs/AddLecture.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.DTOs
+{
+    public class AddLecture
+    {
+        public string fullName { get; set; } = string.Empty;
+        public string staffId { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string phoneNumber { get; set; } = string.Empty;
+        public string faculty { get; set; } = string.Empty;
+        public string department { get; set; } = string.Empty;
+        public string academicRole { get; set; } = string.Empty;
+        public string role { get; set; }= string.Empty;
+        public string password {  get; set; } = string.Empty;
+    }
+}
