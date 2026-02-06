ce78d73cc290d7a0fc6c0c37add4517cca069763
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-06 12:00:07 +0530

    add new model

---
+++ Models/Lectures.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Lectures
+    {
+        [Key]
+        public int id { get; set; }
+        public int user_id {  get; set; }
+        public string full_name {  get; set; }=string.Empty;
+        public string staff_id {  get; set; }= string.Empty;
+        public string email {  get; set; }=string.Empty;
+        public string phone_number {  get; set; }=string.Empty;
+        public string faculty {  get; set; }=string.Empty;
+        public string department {  get; set; }=string.Empty;
+        public string academic_role {  get; set; }=string.Empty;
+    }
+}
