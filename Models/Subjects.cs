a29adaba9c9c2d90c69d098623a1eb70c586b45f
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-06 12:00:30 +0530

    add new model

---
+++ Models/Subjects.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Subjects
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Subject_Name {  get; set; }=string.Empty;
+        public string Subject_Code {  get; set; }=string.Empty;
+        public int Credit { get; set; }
+        public string Academic_Level {  get; set; }=string.Empty;
+        public string Semester {  get; set; }=string.Empty;
+        public string Type { get; set; } = string.Empty;
+
+
+    }
+}
