985feb94dd79d580fa042cdc4b5e16ea02f2d3d7
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-06 11:59:41 +0530

    add new DTO

---
+++ DTOs/AddSubjects.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.DTOs
+{
+    public class AddSubjects
+    {
+        public string subjectCode {  get; set; }=string.Empty;
+        public string subjectName { get; set; } = string.Empty;
+        public int credit {  get; set; }
+        public string academicLevel { get; set; } = string.Empty;
+        public string semester {  get; set; } = string.Empty;
+        public string type {  get; set; } = string.Empty;
+    }
+}
