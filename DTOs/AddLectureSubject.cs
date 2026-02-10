65a30709b91e1615ff9f1cd3c8de7dc76cfdee34
Kavindu-Madhushanka <madu10275@gmail.com>
2026-02-10 17:16:06 +0530

    add new addlecture subject DTOs

---
+++ DTOs/AddLectureSubject.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.DTOs
+{
+    public class AddLectureSubject
+    {
+        public int lecture_id { get; set; }
+        public int subject_id { get; set; }
+        public string subject_type { get; set; }=string.Empty;
+
+    }
+}
